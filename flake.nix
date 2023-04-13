545e981fd97fccf54e122148db31994fa99bcfa4
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-04-13 07:53:21 -0600

    fixup devShell

--- flake.nix
+++ flake.nix
@@ -9,9 +9,9 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = pkgs.mkShell {
+      in {
+        devShells.system = pkgs.mkShell {
+          name = "stm32-on-nix-project"; # rename me
           buildInputs = with pkgs; [
             # bear
             ccls
@@ -22,6 +22,10 @@
             platformio
             stlink
           ];
+          shellHook = ''
+            # update based on your environment and board
+            pio project init --ide emacs --board nucleo_f103rb
+          '';
         };
       });
 }
