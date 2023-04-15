b4fda2f92b8f06e3f0ff263e4072fddaf32a6951
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-04-15 07:40:10 -0600

    add hello world echo example

--- src/main.c
+++ src/main.c
@@ -1,28 +1,28 @@
-/**
+/*
  * @file    main.c
  * @brief   This file contains the "Hello, World." for an STM32F103RB
  * @date    Date of creation: [insert date of creation]
  * @author  [insert author]
  *
- * This module initializes an ADC and sets up a GPIO pin on port A, pin 5 as an
- * output push-pull. The main function then toggles the state of the GPIO pin
- * every 500ms using a busy loop.
+ * This module initializes an the prints out a "Hello, World!" using the UART comms on the stm32
+ * Any characters entered in the serial terminal will be echo'd back
+ * Note: printf is overridden using __write, if not seeing output on the serial comm terminal, use
+ * "\n" to flush the buffer.
  */
 
-#include "stm32f10x.h"
+#include <stdio.h>
+#include <uart.h>
+#include <utils.h>
 
 int main(void) {
-  // Enable GPIOA clock
-  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+  PowerPeripherals();
+  InitLED();
+  InitUart();
 
-  // Set PA05 as output push-pull
-  GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_CNF5);
-  GPIOA->CRL |= GPIO_CRL_MODE5_0;
-
+  printf("Hello, World!\n");
   while (1) {
-    // Toggle PA05 every 500 ms
-    GPIOA->ODR ^= GPIO_ODR_ODR5;
-    for (volatile int i = 0; i < 100000; i++)
-      ;
+    // Echo input over UART
+    Echo();
+    /* Blinky(); */
   }
 }
